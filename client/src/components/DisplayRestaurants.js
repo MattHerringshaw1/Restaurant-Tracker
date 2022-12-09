00ffc44e97a8c8c7c148d344136af4da0f2fce42
MattHerringshaw1 <matt.herringshaw@yahoo.com>
2022-12-09 10:28:49 -0600

    display-restaurants

--- client/src/components/DisplayRestaurants.js
+++ client/src/components/DisplayRestaurants.js
@@ -1,13 +1,48 @@
 import { NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 
 function DisplayRestaurant() {
 
+    const [restaurants, setRestaurants] = useState([])
+
+    const fetchRestaurants = () => {
+        fetch('http://localhost:8080/api/view-restaurants')
+        .then(response => response.json())
+        .then(restaurants => {
+            setRestaurants(restaurants)
+        })
+    }
+
+    useEffect(() => {
+        fetchRestaurants()
+    }, [])
+
+    // handleDelete = () => {
+    //     fetch('http://localhost:8080/delete-restaurant', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({ id })
+    //     })
+    // }
+
+    const restaurantItems = restaurants.map(restaurant => {
+        return <div key={restaurant.id}>
+         <NavLink to = {`/${restaurant.restaurant_name}`}><li>{restaurant.restaurant_name}</li></NavLink>
+         <li>Rating: {restaurant.restaurant_rating}</li>
+         {/* <button onClick={() => handleDelete(restaurant.id)}>Delete</button>  */}
+        </div>
+    })
+
     return(
         <>
             <h1>display restaurants added</h1>
-            <NavLink to = '/view-details'><button>View Details</button></NavLink>
-            <NavLink to = '/edit-list'><button>Edit</button></NavLink>
+
+            <ul>
+                {restaurantItems}
+            </ul>
         </>
     )
 }
--- client/src/components/ViewDetails.js
+++ client/src/components/ViewDetails.js
@@ -1,9 +1,44 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
 
 function ViewDetails() {
+
+    const [restaurant, setRestaurant] = useState([])
+    let {restaurantName} = useParams()
+    console.log(restaurantName)
+
+    const fetchRestaurant = () => {
+        fetch('http://localhost:8080/api/view-restaurants')
+        .then(response => response.json())
+        .then(restaurant => {
+            setRestaurant(restaurant)
+        })
+    }
+
+    useEffect(() => {
+        fetchRestaurant()
+    }, [])
+
+    const filteredRestaurant = restaurant.filter(foodPlace => {
+        return foodPlace.restaurant_name === restaurantName })
+        console.log(filteredRestaurant)
+
+    const restaurantItem = filteredRestaurant.map(restaurant => {
+        return <div key={restaurant.id}>
+         <li>{restaurant.restaurant_name}</li>
+         <li>Located in the intersection of: {restaurant.restaurant_address_1} and {restaurant.restaurant_address_2}</li>
+         <li>Rating: {restaurant.restaurant_rating}</li>
+         
+        </div>
+    })
+
     return (
         <>
             <h1>view details below</h1>
+
+            <ul>{restaurantItem}</ul>
+
             <NavLink to = '/display-list'><button>Back</button></NavLink>
         </>
     )

56a463fa4ac5d1e9951760e02ca3399e343d1f48
MattHerringshaw1 <matt.herringshaw@yahoo.com>
2022-12-14 14:53:12 -0600

    logout

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,29 +1,29 @@
-// import { useEffect } from 'react'
-// import { connect } from 'react-redux'
-// import { useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 
-// function Logout(props) {
+function Logout(props) {
 
-//     const navigate = useNavigate()
+    const navigate = useNavigate()
 
-//     useEffect(() => {
-//         localStorage.removeItem('jwt')
-//         localStorage.removeItem('username')
-//         localStorage.removeItem('userId')
+    useEffect(() => {
+        localStorage.removeItem('jwt')
+        localStorage.removeItem('username')
+        localStorage.removeItem('userId')
 
-//         props.onLogout()
-//         navigate('/login')
-//     })
-//     return (
-//         <h1>logout</h1>
-//     )
-// }
+        props.onLogout()
+        navigate('/login')
+    })
+    return (
+        <h1>logout</h1>
+    )
+}
 
-// const mapDispatchToProps = (dispatch) => {
-//     return {
-//         onLogout: () => dispatch({type: 'ON_LOGOUT'})
-//     }
-// }
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogout: () => dispatch({type: 'ON_LOGOUT'})
+    }
+}
 
-// export default connect(null, mapDispatchToProps)(Logout)
+export default connect(null, mapDispatchToProps)(Logout)
